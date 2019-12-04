3eafd912e48ca1aeef2471565fd62bd993146076
yong ma <stemproaca@hotmail.com>
2019-12-03 22:09:56 -0800

    feedback

--- question2.py
+++ question2.py
@@ -4,7 +4,13 @@ def addUpTo(i):
     for j in range(i):
         sum = sum + j
     print('the sum of the numbers from 1 to', i ,'is', sum)
-
+'''
+on addUpTo(i): 
+    it is close but not quite. 
+    in python, range is [), which means upper bound is exclusive.  
+'''
+    
+    
 #Challenge Number 1
 def Fibonacci(n): 
     if n<2: 
@@ -12,8 +18,46 @@ def Fibonacci(n):
     else: 
         return Fibonacci(n - 1)+Fibonacci(n - 2)
 
+'''
+Fibonacci is: 0,1,1,2,3,5,8,13
+the one you built is a very good approach, although the answer is wrong.
+
+if use recursive: 
+
+---------------------------------
+def Fibonacci(n): 
+    if n<2:
+        return 0
+    elif n==2: 
+        return 1 
+    else: 
+        return Fibonacci(n - 1)+Fibonacci(n - 2)
+---------------------------------
+
+if use dp: 
+---------------------------------
+def Fibonacci_dp(n):
+    fib=[0 for i in range(n+1)]
+    for i in range(1, n+1):
+        if i==1:
+            fib[i]=0
+        elif i==2:
+            fib[2]=1
+        else:
+            fib[i]=fib[i-1]+fib[i-2]
+
+    return fib[n]
+--------------------------------- 
+''' 
+    
+    
+
 def fiboSum(n):
     print('the sum of the first', n ,'fibonacci numbers is', Fibonacci(n+2) - 1)
+'''
+    smart solution. although it is not correct. since your implementation Fib is incorrect
+'''  
+    
 
 #Challenge Number 2
 def triSum(n) :
